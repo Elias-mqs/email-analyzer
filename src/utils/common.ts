8ab974905f0afd537e132de5f062ce59388b4ad5
¨Elias <eliasmqs397@gmail.com>
2025-08-31 10:33:49 -0400

    feat: adding email base url

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,2 +1,3 @@
-// eslint-disable-next-line no-undef
+/* eslint-disable no-undef */
 export const baseUrl: string = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000'
+export const processEmailBaseUrl: string = process.env.NEXT_PUBLIC_EMAIL_API_URL || 'http://localhost:3000'
