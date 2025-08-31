09affa7ff6d0adb0e7a30f54d2b7e6821556cd3e
¨Elias <eliasmqs397@gmail.com>
2025-08-31 09:46:28 -0400

    feat: implement layout and provider components for email analyzer application

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from 'next'
+import React from 'react'
+import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.css'
+import Providers from './provider'
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const metadata: Metadata = {
+  title: 'Email Analyzer',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { EmailClassifier } from '@/components/organisms/email-classifier'
+
+export default function Home() {
+  return (
+    <main className="bg-background min-h-screen">
+      <EmailClassifier />
+    </main>
+  )
+}
